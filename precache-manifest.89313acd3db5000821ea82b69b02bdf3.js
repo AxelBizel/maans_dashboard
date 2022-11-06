371cdd6cb0ccb4368ba4e5e174cc57fccf1ed93f
Axel Bizel <axel.bizel@gmail.com>
2022-11-06 14:32:57 +0100

    Updates

---
+++ precache-manifest.89313acd3db5000821ea82b69b02bdf3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62838766ee8ff41bc74ebbf707e194bb",
+    "url": "/maans_dashboard/index.html"
+  },
+  {
+    "revision": "167a7e0ff8845de15034",
+    "url": "/maans_dashboard/static/css/main.982dfb88.chunk.css"
+  },
+  {
+    "revision": "6fcf7ec8413832932569",
+    "url": "/maans_dashboard/static/js/2.b4dcfdcb.chunk.js"
+  },
+  {
+    "revision": "c68519984d0d767289ceda1ff144e56b",
+    "url": "/maans_dashboard/static/js/2.b4dcfdcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "167a7e0ff8845de15034",
+    "url": "/maans_dashboard/static/js/main.cc3d2ecd.chunk.js"
+  },
+  {
+    "revision": "01e72b4a101cf4a3e2b3",
+    "url": "/maans_dashboard/static/js/runtime-main.30e7b557.js"
+  },
+  {
+    "revision": "7c52984421de00dfafbdd060115f79e3",
+    "url": "/maans_dashboard/static/media/Fauteuil.7c529844.png"
+  }
+]);
